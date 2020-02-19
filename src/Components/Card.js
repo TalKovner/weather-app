9d260deea4da8758caa618198384f5a9c4d8d34b
unknown <elikovner1@gmail.com>
2020-02-20 00:26:57 +0200

    (feat) connect to api

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../style/Card.css';
+import axios from 'axios';
 
+const BneiBrak = 'http://api.openweathermap.org/data/2.5/weather?q=Bnei%20Brak&appid=a8e13f64a58d15932c151dfc791e4d96';
 
-function Card(props) {
-    return (
-        <div className="card">
-            <h4 className='title'>{props.title}</h4>
-        </div>
-    )
+class Card extends Component {
+    constructor() {
+        super()
+        this.state = {
+            temp: 0,
+        }
+    }
+
+    componentDidMount() {
+        axios.get(BneiBrak).then(res => {
+            const weather = res.data;
+            this.setState({ temp: weather.main.temp })
+        })
+    }    
+
+    render() {
+        return (
+            <div className="card">
+                <h4 className='title'>{this.props.title}</h4>
+                <img src="http://openweathermap.org/img/wn/02d@2x.png"></img>
+                <div className="deg-main">{(this.state.temp - 272.15).toFixed(0)}&deg;</div>
+            </div>
+        )
+    }
 }
 
 export default Card;
