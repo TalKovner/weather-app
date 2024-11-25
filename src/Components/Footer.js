0b32df29cc7142dc8f56ce19967d4bb147bac703
TalKovner <61251482+TalKovner@users.noreply.github.com>
2024-11-26 00:12:10 +0200

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,8 @@ import '../style/Footer.css'
 function Footer() {
     return (
         <div className="info">
-            <p>created by Tal Kovner</p>
-            <p>August 2020</p>
+            <p>© Copyright by Tal Kovner</p>
+            <p>2024</p>
         </div>
     )
 }
